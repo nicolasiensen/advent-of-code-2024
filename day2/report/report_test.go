5b2df030299b2748ec19820b33cb03352cb52581
Nícolas Iensen <nicolas.iensen@gmail.com>
2024-12-03 22:28:29 +0100

    Add tests for report module (day2)

---
+++ day2/report/report_test.go
@@ -0,0 +1,65 @@
+package report
+
+import (
+	"testing"
+)
+
+func TestSafeReportWithLevelsDecreasing(t *testing.T) {
+	rp := []int{7, 6, 4, 2, 1}
+	expect := true
+	result := Safe(rp)
+
+	if expect != result {
+		t.Fatalf(`Safe(%v) = %v but it should be %v`, rp, result, expect)
+	}
+}
+
+func TestUnsafeReportWithIncreaseOf5(t *testing.T) {
+	rp := []int{1, 2, 7, 8, 9}
+	expect := false
+	result := Safe(rp)
+
+	if expect != result {
+		t.Fatalf(`Safe(%v) = %v but it should be %v`, rp, result, expect)
+	}
+}
+
+func TestUnsafeReportWithDecreaseOf4(t *testing.T) {
+	rp := []int{9, 7, 6, 2, 1}
+	expect := false
+	result := Safe(rp)
+
+	if expect != result {
+		t.Fatalf(`Safe(%v) = %v but it should be %v`, rp, result, expect)
+	}
+}
+
+func TestUnsafeReportWithIncreaseThenDecrease(t *testing.T) {
+	rp := []int{1, 3, 2, 4, 5}
+	expect := false
+	result := Safe(rp)
+
+	if expect != result {
+		t.Fatalf(`Safe(%v) = %v but it should be %v`, rp, result, expect)
+	}
+}
+
+func TestUnsafeReportWithNeitherIncreaseOrDescrease(t *testing.T) {
+	rp := []int{8, 6, 4, 4, 1}
+	expect := false
+	result := Safe(rp)
+
+	if expect != result {
+		t.Fatalf(`Safe(%v) = %v but it should be %v`, rp, result, expect)
+	}
+}
+
+func TestSafeReportWithLevelsIncreasing(t *testing.T) {
+	rp := []int{1, 3, 6, 7, 9}
+	expect := true
+	result := Safe(rp)
+
+	if expect != result {
+		t.Fatalf(`Safe(%v) = %v but it should be %v`, rp, result, expect)
+	}
+}
