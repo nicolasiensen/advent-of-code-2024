4dfc39a17fd844f70dcfeeb245974e5370c8bdb9
Nícolas Iensen <nicolas.iensen@gmail.com>
2024-12-08 21:14:11 +0100

    Solve day 3 part 2

--- day3/day3.go
+++ day3/day3.go
@@ -13,27 +13,35 @@ func main() {
 		println(err)
 	}
 
-	mulRegexp, _ := regexp.Compile(`mul\(\d{1,3},\d{1,3}\)`)
+	mulRegexp, _ := regexp.Compile(`mul\(\d{1,3},\d{1,3}\)|don't\(\)|do\(\)`)
 	ops := mulRegexp.FindAllString(string(file), -1)
 
 	digRegexp, _ := regexp.Compile(`\d{1,3}`)
 	sum := 0
+	enabled := true
 
 	for i := range ops {
 		op := ops[i]
-		args := digRegexp.FindAllString(op, -1)
 
-		op1, err := strconv.Atoi(args[0])
-		if err != nil {
-			fmt.Println(err)
+		if op == "don't()" {
+			enabled = false
+		} else if op == "do()" {
+			enabled = true
+		} else if enabled {
+			args := digRegexp.FindAllString(op, -1)
+
+			op1, err := strconv.Atoi(args[0])
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			op2, err := strconv.Atoi(args[1])
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			sum += op1 * op2
 		}
-
-		op2, err := strconv.Atoi(args[1])
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		sum += op1 * op2
 	}
 
 	fmt.Println("Result:", sum)
