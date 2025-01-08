3edb7a0b7c0c93a01ef8a9d8459d59defd497df0
Nícolas Iensen <nicolas.iensen@gmail.com>
2025-01-08 02:19:36 +0100

    Refactor Guard to use Vector

--- day6/guard.go
+++ day6/guard.go
@@ -1,78 +1,74 @@
 package main
 
-import (
-	"fmt"
-)
-
 type Guard struct {
-	Positions []Point
-	Bearing   Bearing
+	Vectors []Vector
 }
 
 func BuildGuard(position Point, bearing Bearing) Guard {
-	return Guard{[]Point{position}, bearing}
+	return Guard{[]Vector{{position, bearing}}}
 }
 
-func (g Guard) LastPos() Point {
-	return g.Positions[len(g.Positions)-1]
+func (g Guard) Position() Point {
+	return g.Vectors[len(g.Vectors)-1].Point
 }
 
 func (g Guard) IsWithinBounds(width int, height int) bool {
-	lastPos := g.LastPos()
+	lastPos := g.Position()
 	return lastPos.X > 0 && lastPos.X < width && lastPos.Y > 0 && lastPos.Y < height
 }
 
 func (g Guard) UniquePositions() []Point {
 	var uniquePositions []Point
-	for i, p := range g.Positions {
+	for _, v := range g.Vectors {
 		unique := true
 
 		for _, up := range uniquePositions {
-			if p == up {
+			if v.Point == up {
 				unique = false
 			}
 		}
 
 		if unique {
-			fmt.Printf("Unique position: %v, %d\n", p, i)
-			uniquePositions = append(uniquePositions, p)
+			uniquePositions = append(uniquePositions, v.Point)
 		}
 	}
 	return uniquePositions
 }
 
 func (g *Guard) Move() {
-	g.Positions = append(g.Positions, g.NextPos())
+	g.Vectors = append(g.Vectors, g.NextVector())
 }
 
-func (g Guard) NextPos() Point {
-	nextPos := Point{g.LastPos().X, g.LastPos().Y}
+func (g Guard) NextVector() Vector {
+	nextVec := Vector{g.Position(), g.Bearing()}
 
-	switch g.Bearing {
+	switch g.Bearing() {
 	case North:
-		nextPos.Y -= 1
+		nextVec.Point.Y -= 1
 	case East:
-		nextPos.X += 1
+		nextVec.Point.X += 1
 	case South:
-		nextPos.Y += 1
+		nextVec.Point.Y += 1
 	case West:
-		nextPos.X -= 1
+		nextVec.Point.X -= 1
 	}
 
-	return nextPos
+	return nextVec
 }
 
 func (g *Guard) Turn() Bearing {
-	g.Bearing += 1
-	if g.Bearing > 3 {
-		g.Bearing = 0
+	bearing := g.Bearing()
+	bearing += 1
+	if bearing > 3 {
+		bearing = 0
 	}
-	return g.Bearing
+	g.Vectors = append(g.Vectors, Vector{g.Position(), bearing})
+	return bearing
 }
 
 func (g *Guard) Patrol(width int, height int, obstacles []Point) {
 	for g.IsWithinBounds(width, height) {
-		nextPos := g.NextPos()
+		nextPos := g.NextVector().Point
 
 		for _, obstacle := range obstacles {
 			if obstacle == nextPos {
@@ -83,4 +79,8 @@ func (g *Guard) Patrol(width int, height int, obstacles []Point) {
 
 		g.Move()
 	}
+}
+
+func (g Guard) Bearing() Bearing {
+	return g.Vectors[len(g.Vectors)-1].Bearing
 }
